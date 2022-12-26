d2cd6214aaab3c17e23271c841d233f4a863745a
bomii1 <buij304@naver.com>
2022-12-27 01:52:45 +0900

    1-1

--- chap1/main.c
+++ chap1/main.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    int a,b,c,d,result;
+    int a,b,result,option;
 
-    printf("첫 번째 값: ");
+    printf("첫번째 값 = ");
     scanf("%d", &a);
-    printf("두 번째 값: ");
+    printf("<1> 덧셈  <2> 뺄셈  <3> 곱셈  <4> 나눗셈  ==> ");
+    scanf("%d", &option);
+    printf("두번째 값 = ");
     scanf("%d", &b);
-    printf("세 번째 값: ");
-    scanf("%d", &c);
-    printf("네 번째 값: ");
-    scanf("%d", &d);
 
-    result = a + b + c +d;
-    printf("a + b + c + d = %d",result);
+    if (option == 1) { result = a + b; printf("a + b = %d",result); }
+    else if (option == 2) { result = a - b; printf("a - b = %d",result); }
+    else if (option == 3) { result = a * b; printf("a * b = %d",result); }
+    else if (option == 4) { result = a % b; printf("a / b = %d",result);; }
+    else { printf("잘못된 번호입니다"); }
 
     return 0;
 }
