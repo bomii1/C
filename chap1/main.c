cbb3a574b26357a60bfaf84580c4a1e8fa39e769
bomii1 <buij304@naver.com>
2022-12-27 01:56:02 +0900

    1-1

--- chap1/main.c
+++ chap1/main.c
@@ -8,14 +8,14 @@ int main() {
     scanf("%d", &a);
     printf("<1> 덧셈  <2> 뺄셈  <3> 곱셈  <4> 나눗셈  ==> ");
     scanf("%d", &option);
+    if (option > 4 || option < 1) { printf("잘못된 번호입니다"); return 0; }
     printf("두번째 값 = ");
     scanf("%d", &b);
 
     if (option == 1) { result = a + b; printf("a + b = %d",result); }
     else if (option == 2) { result = a - b; printf("a - b = %d",result); }
     else if (option == 3) { result = a * b; printf("a * b = %d",result); }
-    else if (option == 4) { result = a % b; printf("a / b = %d",result);; }
-    else { printf("잘못된 번호입니다"); }
+    else if (option == 4) { result = a % b; printf("a / b = %d",result); }
 
     return 0;
 }
