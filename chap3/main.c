93ac83022bab52e8fd58cfd6e42bba6f5335a5c3
bomii1 <buij304@naver.com>
2022-12-29 14:42:21 +0900

    3-5

--- chap3/main.c
+++ chap3/main.c
@@ -1,23 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int option, num;
 
-    printf("입력 진수 결정 <1>10  <2>16  <3>8 :");
-    scanf("%d", &option);
-    printf("값 입력: ");
+    char str[10];
 
-    if (option == 1) {
-        scanf("%d", &num);
-    } else if (option == 2) {
-        scanf("%X", &num);
-    } else {
-        scanf("%o", &num);
+    printf("문자열을 입력 ==>");
+    scanf("%s", str);
+    printf("%s\n", str);
+
+    for (int i=sizeof(str)-1; i>=0; i--) {
+        if (str[i] == NULL) { continue;}
+        printf("%c",str[i]);
     }
-
-    printf("10진수 ==> %d\n", num);
-    printf("16진수 ==> %x\n", num);
-    printf("8진수 ==> %o\n", num);
 
 
     return 0;
