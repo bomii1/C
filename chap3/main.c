a179ae87ae6ec7cac907b3c76463ca02f1fa9b55
bomii1 <buij304@naver.com>
2022-12-31 00:41:18 +0900

    5-10

--- chap3/main.c
+++ chap3/main.c
@@ -3,7 +3,7 @@
 int main() {
 
     int year;
-    printf("년도를 입력하세요: ");
+    printf("년도를 입력하세요:");
     scanf("%d", &year);
 
     if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
