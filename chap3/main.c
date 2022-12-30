43de9100e946d7ca0093015024943ca7aa25014d
bomii1 <buij304@naver.com>
2022-12-31 00:33:09 +0900

    5-9

--- chap3/main.c
+++ chap3/main.c
@@ -2,18 +2,20 @@
 
 int main() {
 
-    float firstNum, secondNum;
+    int money, num;
 
-    printf("첫 번째 계산할 값 ==> ");
-    scanf("%f", &firstNum);
-    printf("두 번째 계산할 값 ==> ");
-    scanf("%f", &secondNum);
+    printf("## 교환할 돈은? ");
+    scanf("%d", &money);
 
-    printf("%5.2f + %5.2f = %5.2f\n", firstNum, secondNum, firstNum + secondNum);
-    printf("%5.2f - %5.2f = %5.2f\n", firstNum, secondNum, firstNum - secondNum);
-    printf("%5.2f * %5.2f = %5.2f\n", firstNum, secondNum, firstNum * secondNum);
-    printf("%5.2f / %5.2f = %5.2f\n", firstNum, secondNum, firstNum / secondNum);
-    printf("%d %% %d = %d\n", (int)firstNum, (int)secondNum, (int)firstNum % (int)secondNum);
+    printf("오백원짜리 ==> %d\n", money / 500);
+    int charge = money % 500;
+    printf("백원짜리 ==> %d\n", charge / 100);
+    charge %= 100;
+    printf("오십원짜리 ==> %d\n", charge / 50);
+    charge %= 50;
+    printf("십원짜리 ==> %d\n", charge / 10);
+    charge %= 10;
+    printf("바꾸지 못한 잔돈 ==> %d", charge);
 
 
     return 0;
