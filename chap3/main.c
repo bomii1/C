67262af634aa96f6f3d8f0f8b6bfa7cbe47a3f9f
bomii1 <buij304@naver.com>
2023-01-02 11:00:54 +0900

    6-15

--- chap3/main.c
+++ chap3/main.c
@@ -2,38 +2,15 @@
 
 int main() {
 
-    int a, b, result;
-    char oper;
-
-    printf("첫번째 수 입력:");
-    scanf("%d", &a);
-    printf("연산자 입력: ");
-    /*
-    정수를 입력하고 나서 enter을 누르면 %c에 문자 대신 \n 이 들어가게 됨
-    1. %c 앞에 공백을 넣어주면 \n 무시됨, \n 이 공백으로 인식되어 무시되는 것
-    2. rewind(stdin) 함수 사용, 매게 변수로 들어온 스트림을 초기화
-    3. fflush(stdin) 함수 사용, 시스템 버퍼 값을 지우는 함수
-    */
-    scanf(" %c", &oper);
-    printf("두번째 수 입력: ");
-    scanf("%d", &b);
-
-    switch (oper) {
-        case '+': result = a + b;
-            break;
-        case '-': result = a - b;
-            break;
-        case '*': result = a * b;
-            break;
-        case '/': result = a / b;
-            break;
-        case '%': result = a % b;
-            break;
-        default:
-            printf("다시 입력하세요");
+    int a = 2;
+    switch (a) {
+        case 1:
+            printf("1이다");
+        case 2:
+            printf("2이다");
+        case 3:
+            printf("3이다"); 
     }
-    printf("결과는 %d", result);
-
 
     return 0;
 }
