569514c26d223775a0cd331212264f72d29fb65c
bomii1 <buij304@naver.com>
2023-01-03 15:13:52 +0900

    7-18

--- chap7/main.c
+++ chap7/main.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 
-    int start, end, num;
+    char str[100];
+    int big = 0;
+    int small = 0;
+    int num = 0;
+    int ch = 0;
 
-    printf("합계의 시작값: ");
-    scanf("%d", &start);
-    printf("합계의 끝값: ");
-    scanf("%d", &end);
-    printf("배수: ");
-    scanf("%d", &num);
+    printf("문자열을 입력 (100자 이내): ");
+    scanf("%s", str);
 
-    int hap = 0;
-    for (int i=start; i<=end; i++) {
-        if (i % num == 0) {
-            hap += i;
+    for (int i=0; i<strlen(str); i++) {
+        if (str[i] >= 'A' && str[i] <= 'Z') {
+            big++;
+        } else if (str[i] >= 'a' && str[i] <= 'z') {
+            small++;
+        } else if (str[i] >= '0' && str[i] <= '9') {
+            num++;
+        } else {
+            ch++;
         }
     }
-    printf("합계 = %d", hap);
+
+    printf("대문자 %d 개, 소문자 %d 개, 숫자 %d 개, 특수문자 %d 개", big, small, num, ch);
 
     return 0;
 }
