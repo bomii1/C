e99a90ad10b9606334b7519201f65d457a09eb27
bomii1 <buij304@naver.com>
2023-01-05 09:50:58 +0900

    8-23

--- chap8/main.c
+++ chap8/main.c
@@ -3,17 +3,23 @@
 
 int main() {
 
-    int gugudan[8][9];
+    char str[50];
+    char ch1, ch2;
 
-    for (int i=1; i<=9; i++) {
-        for (int k=2; k<=9; k++) {
-            gugudan[i][k] = i * k;
-            printf("%d x %2d = %2d  ", k, i, gugudan[i][k]);
+    printf("글자 입력: ");
+    gets(str);
+    printf("기존의 문자와 새로운 문자: ");
+    scanf("%c %c", &ch1, &ch2);
+    printf("변환된 결과: ");
+
+    for (int i=0; i<strlen(str); i++) {
+        if (str[i] == ch1) {
+            str[i] = ch2;
+            printf("%c", str[i]);
+        } else {
+            printf("%c", str[i]);
         }
-        printf("%\n");
     }
-
-
 
     return 0;
 }
