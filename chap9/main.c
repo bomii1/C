7eb43356e046fa57bd337dd1dd7f50a78e2b8741
bomii1 <buij304@naver.com>
2023-01-05 21:16:29 +0900

    test 9-9

--- chap9/main.c
+++ chap9/main.c
@@ -3,21 +3,27 @@
 
 int main() {
 
-    int arr[10] = {1,0,3,2,5,4,7,6,9,8};
+    int arr[10];
     int *p;
     p = arr;
 
+    int size = sizeof(arr)/sizeof(int);
+    for (int i=0; i<size; i++) {
+        printf("arr[%d] 데이터: ", i);
+        scanf("%d", p+i);
+    }
+
     printf("정렬 전 배열 => ");
-    for (int i=0; i<sizeof(arr)/sizeof(int); i++) {
+    for (int i=0; i<size; i++) {
         printf("%2d", *(p+i));
     }
     printf("\n");
 
     printf("정렬 후 배열 => ");
 
-    for (int i=0; i<sizeof(arr)/sizeof(int); i++) {
-          for (int k=i+1; k<sizeof(arr)/sizeof(int); k++) {
-              if (*(p+i) < *(p+k)) {
+    for (int i=0; i<size; i++) {
+          for (int k=i+1; k<size; k++) {
+              if (*(p+i) > *(p+k)) {
                   int tmp = *(p+i);
                   *(p+i) = *(p+k);
                   *(p+k) = tmp;
@@ -25,7 +31,7 @@ int main() {
           }
     }
 
-    for (int i=0; i<sizeof(arr)/sizeof(int); i++) {
+    for (int i=0; i<size; i++) {
         printf("%2d", *(p+i));
     }
 
