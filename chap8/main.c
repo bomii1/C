1b29d127ceaecbd2a1f79939465902cf4be06f61
bomii1 <buij304@naver.com>
2023-01-05 09:03:59 +0900

    8-20

--- chap8/main.c
+++ chap8/main.c
@@ -4,13 +4,16 @@
 int main() {
 
     char str[100];
+    char strCopy[100];
 
     printf("문자열을 입력하세요: ");
     scanf("%s", str);
     printf("내용을 거꾸로 출력: ");
-    for (int i=strlen(str); i>=0; i--) {
-        printf("%c", str[i]);
+    for (int i=0; i<strlen(str); i++) {
+        strCopy[i] = str[strlen(str)-i-1];
     }
+    strCopy[strlen(str)+1] = '\0';
+    printf("%s",strCopy);
 
 
     return 0;
