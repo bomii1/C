c3c9078761b9efd928ca8a13293f22a5fde71a2a
bomii1 <buij304@naver.com>
2023-01-10 23:31:37 +0900

    exam 11-8

---
+++ chap11/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+    char s[200];
+    FILE *rfp;
+
+    rfp = fopen("C:\\Users\\buij3\\OneDrive\\바탕 화면\\testC.txt", "r");
+
+    for( ; ; ) {
+        fgets(s, 200, rfp);
+
+        if (feof(rfp)) {
+            break;
+        }
+        printf("%s", s);
+    }
+    fclose(rfp);
+
+    fclose(rfp);
+
+    return 0;
+}
+
